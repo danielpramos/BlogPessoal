f084003ac75587ecc9399261ed53466f0552a477
Daniel Pinheiro Ramos <daniel.ramos5291@gmail.com>
2022-01-28 17:27:41 -0300

    Update Pom.xml / Created Packages with Class - Security | Service / Class - Usuario | UsuarioLogin | UsuarioRepository.

--- src/main/java/br/org/generation/blogpessoal/repository/TemaRepository.java
+++ src/main/java/br/org/generation/blogpessoal/repository/TemaRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.org.generation.blogpessoal.model.Postagem;
 import br.org.generation.blogpessoal.model.Tema;
 
 @Repository
